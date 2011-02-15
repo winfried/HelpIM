ac8a4def84ce8a68ed53bc16586fbe231e8dc70c
Stefan Strigler <stefan@strigler.de>
2011-02-15 08:54:12 +0100

    playing around with the admin

--- django/helpim/groups/admin.py
+++ django/helpim/groups/admin.py
@@ -1,7 +1,14 @@
 from groups.models import Group, Meeting, Member, MeetingParticipant
 from django.contrib import admin
 
-admin.site.register(Group)
+class MemberInline(admin.TabularInline):
+    model = Member
+    extra = 0
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [MemberInline]
+
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Member)
 admin.site.register(Meeting)
 admin.site.register(MeetingParticipant)
