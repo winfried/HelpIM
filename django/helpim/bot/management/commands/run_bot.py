4dfcc9fa33645ee92eca59dd50313700b2794a29
Stefan Strigler <stefan@strigler.de>
2011-04-07 18:21:17 +0200

    some comments for todo

--- django/helpim/bot/management/commands/run_bot.py
+++ django/helpim/bot/management/commands/run_bot.py
@@ -5,6 +5,12 @@ class Command(BaseCommand):
     help = "runs the jabber channel bot"
 
     def handle(self, *args, **options):
+        """ get config """
+
+        
+        """ pass config to bot """
         bot = Bot()
+
+        """ run the bot """
         bot.run()
         pass
