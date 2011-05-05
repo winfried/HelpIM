4d49ad621ea182d0f348ce3681913c676c145899
Filip Noetzel <filip@j03.de>
2011-05-05 13:00:56 +0200

    rooms: Add client workflow

--- helpim/rooms/views.py
+++ helpim/rooms/views.py
@@ -6,6 +6,8 @@ from django.db import transaction
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.shortcuts import render_to_response
+from django.forms import Form, CharField
+from django.core.context_processors import csrf
 
 @transaction.commit_on_success
 @login_required
@@ -49,4 +51,46 @@ def staff_join_chat(request, room_pk=None):
       }, indent=2)
     })
 
+class GetClientNickForm(Form):
+    nick = CharField(max_length=40)
+
+@transaction.commit_on_success
+def client_join_chat(request):
+    try:
+      room = One2OneRoom.objects.filter(status__exact='staffWaiting')[:1][0]
+    except IndexError:
+      return redirect('/rooms/unavailable/')
+
+    if request.method != 'POST':
+        form = GetClientNickForm()
+    else:
+        form = GetClientNickForm(request.POST)
+
+    if not form.is_valid():
+        c = { 'form': form }
+        c.update(csrf(request))
+        return render_to_response('rooms/client_get_info.html', c)
+
+    return render_to_response(
+      'rooms/client_join_chat.html', {
+      'debug': settings.DEBUG,
+      'xmpptk_config': dumps({
+          'httpbase': "/http-bind/",
+          'authtype': "saslanon",
+          'domain': settings.BOT['connection']['domain'],
+          'muc_service': room.getRoomService(),
+          'muc_room': room.getRoomId(),
+          'muc_password': room.password,
+          'muc_nick': form.cleaned_data['nick'],
+          'mode': 'light',
+          'logout_redirect': '/rooms/logged_out/',
+          'bot_nick': settings.BOT['muc']['nick'],
+      }, indent=2)
+    })
+
+def client_logged_out(request):
+    return render_to_response('rooms/client_logged_out.html', {});
+
+def client_room_unavailable(request):
+    return render_to_response('rooms/client_room_unavailable.html', {});
 
