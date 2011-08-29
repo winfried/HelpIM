165e92f2bb9c88e8c0dbe0c38aed761a58ae2814
Stefan Strigler <stefan@strigler.de>
2011-08-29 13:07:09 +0200

    fix after renaming BlockList to BlockedIP

--- helpim/rooms/admin.py
+++ helpim/rooms/admin.py
@@ -1,4 +1,4 @@
-from helpim.rooms.models import One2OneRoom, BlockList
+from helpim.rooms.models import One2OneRoom, BlockedIP
 from django.contrib import admin
 
 class One2OneRoomAdmin(admin.ModelAdmin):
@@ -10,6 +10,6 @@ class One2OneRoomAdmin(admin.ModelAdmin):
 
 admin.site.register(One2OneRoom, One2OneRoomAdmin)
 
-class BlockListAdmin(admin.ModelAdmin):
-    model = BlockList
-admin.site.register(BlockList, BlockListAdmin)
+class BlockedIPAdmin(admin.ModelAdmin):
+    model = BlockedIP
+admin.site.register(BlockedIP, BlockedIPAdmin)
