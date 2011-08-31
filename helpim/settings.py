e163f3773d4dc67d6561a6c5107969aa531a3d58
Stefan Strigler <stefan@strigler.de>
2011-08-31 13:04:28 +0200

    removed helpim.groups from installed apps

--- helpim/settings.py
+++ helpim/settings.py
@@ -123,7 +123,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'helpim.conversations',
-    'helpim.groups',
+#    'helpim.groups',
     'helpim.rooms',
     'helpim',
     'south',
