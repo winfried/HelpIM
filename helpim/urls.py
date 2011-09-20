45bd6fb8c99433eff707cbdbc2bd4e45f36bb5f6
Winfried Tilanus <winfried@tilanus.com>
2011-09-20 02:00:24 +0200

    Adding a redirect from /login(/) to /admin/

--- helpim/urls.py
+++ helpim/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
@@ -19,6 +20,8 @@ urlpatterns += patterns(
 
     url(r'^$', 'helpim.rooms.views.client_join_chat', name='client_join_chat'),
 
+    url(r'^login/?$', redirect_to, {'url': '/admin/'}),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/threadedcomments/', include('threadedcomments.urls')),
