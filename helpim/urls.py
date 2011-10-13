2576653e41eeeb24f3f5a394e6c72e34225f1dcb
Stefan Strigler <stefan@strigler.de>
2011-10-13 12:19:47 +0200

    pass id of new form entry to form_sent template

--- helpim/urls.py
+++ helpim/urls.py
@@ -20,6 +20,8 @@ urlpatterns += patterns(
 
     url(r'^$', 'helpim.rooms.views.client_join_chat', name='client_join_chat'),
 
+    url(r"^forms/(?P<slug>.*)/(?P<entry>.*)/sent/$", "helpim.questionnaire.views.form_sent", kwargs={'template':'questionnaire/form_sent.html'}, name="form_sent"),
+    url(r"^forms/(?P<slug>.*)/$", "helpim.questionnaire.views.form_detail", name="form_detail"),
 
     url(r'^login/?$', redirect_to, {'url': '/admin/'}),
     url(r'^login/$', 'django.contrib.auth.views.login'),
@@ -30,10 +32,4 @@ urlpatterns += patterns(
     url(r'^admin/rooms/join/$', 'helpim.rooms.views.staff_join_chat', name='staff_join_room'),
 
     url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
-)
-
-urlpatterns += patterns(
-    'forms_builder.forms.views',
-    url(r"^forms/(?P<slug>.*)/sent/$", "form_sent", kwargs={'template':'questionnaire/form_sent.html'}, name="form_sent"),
-    url(r"^forms/(?P<slug>.*)/$", "form_detail", name="form_detail"),
 )
