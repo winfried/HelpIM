8587bfd2bff4788b3481fb8764553bd836c49007
Stefan Strigler <stefan@strigler.de>
2011-10-14 17:50:19 +0200

    set table engine to InnoDB per default

--- helpim/settings.py
+++ helpim/settings.py
@@ -16,7 +16,10 @@ DATABASES = {
         'PASSWORD': 'topSecret',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+        'OPTIONS': {
+            'init_command': 'SET storage_engine=INNODB',
+        },
+    },
 }
 
 # Add fixtures dir for fixtures over al submodules
