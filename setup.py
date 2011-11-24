3eedf9ab0bef172cadc6d0151cc7f4c27ece311b
Winfried Tilanus <winfried@tilanus.com>
2011-11-24 20:42:51 +0100

    Second go at making the egg complete, adapting the upgrade script.

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 
-import sys, os
 from setuptools import setup, find_packages
-from os.path import join, dirname, relpath
 from version import get_git_version
 
-sys.path.append(join(dirname(__file__), 'helpim'))
-
-long_description=(
-    open(join(dirname(__file__),
-         'README.rst',
-         )).read())
+long_description=(open('README.rst').read())
 
 name='helpim'
 
@@ -27,24 +20,6 @@ install_requires=[
    'libxml2-python==2.6.21',
    ]
 
-include_dirs = [
-    'static',
-    'helpim/locale',
-    'helpim/templates',
-    'helpim/fixtures',
-    'helpim/questionnaire/templates/forms',
-    'helpim/doc/debian/example',
-    ]
-
-static_files = []
-for include_dir in include_dirs:
-    for root, dirs, files in os.walk(join(dirname(__file__), include_dir)):
-        relroot = relpath(root, dirname(__file__))
-        static_files.append((
-          join('share', 'helpim', relroot),
-          [join(relroot, f) for f in files]
-        ))
-
 setup(
     name=name,
     version=get_git_version().lstrip('v'),
@@ -56,11 +31,10 @@ setup(
     author_email='helpdesk@e-hulp.nl',
     packages=find_packages('.'),
     package_dir={'': '.'},
-    data_files=static_files,
-    namespace_packages=[],
-    include_package_data = True,
     install_requires=install_requires,
     zip_safe = False,
+    namespace_packages=[],
+    include_package_data = True,
     classifiers = [
       "Programming Language :: Python",
       "Development Status :: 4 - Beta",
