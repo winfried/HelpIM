ecd1f8e5b052f4e4ba0f647ae3d02081bfec73b7
Stefan Strigler <stefan@strigler.de>
2011-12-16 14:35:31 +0100

    don't allow to edit fields 'user', 'ready' and 'activation_key'

--- helpim/buddychat/admin.py
+++ helpim/buddychat/admin.py
@@ -6,5 +6,6 @@ class BuddyChatProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'ready', 'volunteer')
     list_filter = ('ready', 'volunteer')
     list_editable = ('volunteer',)
+    readonly_fields = ('user', 'ready', 'activation_key')
 
 admin.site.register(BuddyChatProfile, BuddyChatProfileAdmin)
