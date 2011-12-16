2c7e11c693498b210dc25565737bf6daff5b5cc8
Stefan Strigler <stefan@strigler.de>
2011-12-16 14:58:45 +0100

    nice formatting of chat buddy admin view

--- helpim/buddychat/admin.py
+++ helpim/buddychat/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from helpim.buddychat.models import BuddyChatProfile
 
 class BuddyChatProfileAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'activation_key', 'ready', 'volunteer')
+            })
+        )
+
     list_display = ('user', 'ready', 'volunteer')
     list_filter = ('ready', 'volunteer')
     list_editable = ('volunteer',)
