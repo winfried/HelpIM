c75788e8e270c4fb69c06e7e5e6e6cacab9f8344
Stefan Strigler <stefan@strigler.de>
2011-12-16 16:23:26 +0100

    include link to personal page

--- helpim/buddychat/admin.py
+++ helpim/buddychat/admin.py
@@ -11,11 +11,16 @@ class BuddyChatProfileAdmin(admin.ModelAdmin):
             }),
         )
 
-    list_display = ('user', 'ready', 'volunteer')
+    list_display = ('user', 'ready', 'volunteer', 'coupled_at', 'personal_page')
     list_filter = ('ready', 'volunteer')
     list_editable = ('volunteer',)
     readonly_fields = ('user', 'ready', 'activation_key', 'coupled_at')
 
+    def personal_page(self, obj):
+        return '<a href="/profile/%s/">View</a>' % obj.user.username
+    personal_page.short_description = 'Personal Page'
+    personal_page.allow_tags = True
+    
     def save_model(self, request, obj, form, change):
         if change and 'volunteer' in form.changed_data:
             if obj.volunteer is None:
