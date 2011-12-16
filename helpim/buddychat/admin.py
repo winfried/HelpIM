f0435ba6f8841189c4846d68be0e0b959f203438
Stefan Strigler <stefan@strigler.de>
2011-12-16 13:52:29 +0100

    some initial admin configuration for chat buddies

--- helpim/buddychat/admin.py
+++ helpim/buddychat/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from helpim.buddychat.models import BuddyChatProfile
 
 class BuddyChatProfileAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'ready', 'volunteer')
+    list_filter = ('ready', 'volunteer')
+    list_editable = ('volunteer',)
 
-admin.site.register(BuddyChatProfile)
+admin.site.register(BuddyChatProfile, BuddyChatProfileAdmin)
