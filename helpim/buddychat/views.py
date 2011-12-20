0fd3ef9ac4bb28f786dee1fb4bddfcc9ff5f5ac9
Stefan Strigler <stefan@strigler.de>
2011-12-20 13:47:10 +0100

    fixed imports

--- helpim/buddychat/views.py
+++ helpim/buddychat/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
-from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 
 def welcome(request):
