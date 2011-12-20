8a3e1cc2982b3132d7f56d71bd5b450fd5cf027a
Stefan Strigler <stefan@strigler.de>
2011-12-20 18:10:31 +0100

    populate template with volunteers

--- helpim/buddychat/views.py
+++ helpim/buddychat/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
@@ -17,10 +18,14 @@ def welcome(request):
 def profile(request, username):
     client = get_object_or_404(BuddyChatProfile, user__username = username)
 
+    params = {'client': client}
+    if request.user.has_perm('buddychat.is_coordinator'):
+        params['volunteers'] = User.objects.filter(groups__name='volunteers')
+
     if request.user.has_perm('buddychat.is_coordinator') or (request.user.has_perm('buddychat.is_volunteer') and request.user == client.volunteer) or request.user == client.user:
         return render_to_response(
             'buddychat/profile.html',
-            {'client': client},
+            params,
             context_instance=RequestContext(request)
             )
     return HttpResponse(_('Access Denied'))
