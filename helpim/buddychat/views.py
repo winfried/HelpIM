cd01a4a006dd758553e2a70d6ff4269b812be872
Stefan Strigler <stefan@strigler.de>
2011-12-20 17:25:44 +0100

    added access control

--- helpim/buddychat/views.py
+++ helpim/buddychat/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.utils.translation import ugettext as _
 
 from helpim.buddychat.models import BuddyChatProfile
 
@@ -15,10 +16,11 @@ def welcome(request):
 @login_required
 def profile(request, username):
     client = get_object_or_404(BuddyChatProfile, user__username = username)
-    
-    return render_to_response(
-        'buddychat/profile.html',
-        {'client': client},
-        context_instance=RequestContext(request)
-        )
-    
+
+    if request.user.has_perm('buddychat.is_coordinator') or (request.user.has_perm('buddychat.is_volunteer') and request.user == client.volunteer) or request.user == client.user:
+        return render_to_response(
+            'buddychat/profile.html',
+            {'client': client},
+            context_instance=RequestContext(request)
+            )
+    return HttpResponse(_('Access Denied'))
