583e25f05f8e217b534e3185e377717fca6c4688
splatte <sp@x-berg.de>
2012-02-16 15:58:34 +0100

    Revert "fix: checking app order only necessary when helpim.buddychat is loaded"
    This reverts commit a60d5b72b3f7aedff2fcc81a8b03bc720ed4fb2a.

--- helpim/buddychat/models.py
+++ helpim/buddychat/models.py
@@ -14,12 +14,11 @@ class ConfigurationError(Exception):
     def __str__(self):
         return repr(self.value)
 
-""" if helpim.buddychat is loaded, require we're loaded before helpim.rooms and helpim.questionnaires """
+""" check if we're loaded before helpim.rooms and helpim.questionnaires """
 from django.conf import settings
 ia = settings.INSTALLED_APPS
-if 'helpim.buddychat' in ia:
-    if ia.index('helpim.buddychat') > ia.index('helpim.rooms') or ia.index('helpim.buddychat') > ia.index('helpim.questionnaire'):
-        raise ConfigurationError('bad order of INSTALLED_APPS: helpim.buddychat must be loaded before helpim.questionnaire and helpim.rooms')
+if ia.index('helpim.buddychat') > ia.index('helpim.rooms') or ia.index('helpim.buddychat') > ia.index('helpim.questionnaire'):
+    raise ConfigurationError('bad order of INSTALLED_APPS: helpim.buddychat must be loaded before helpim.questionnaire and helpim.rooms')
 
 from helpim.common.models import register_position_choices
 register_position_choices([
