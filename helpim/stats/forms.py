f183cea61ee2609607395a299e39960707d734f9
splatte <sp@x-berg.de>
2012-03-06 19:31:37 +0100

    brush up form a bit

--- helpim/stats/forms.py
+++ helpim/stats/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms.extras.widgets import SelectDateWidget
+from django.utils.translation import ugettext as _
 
 from helpim.stats.models import Report
 
@@ -6,3 +8,14 @@ from helpim.stats.models import Report
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
+        widgets = {
+            # TODO: use `years`:List available years in selection
+            'period_start': SelectDateWidget(),
+            'period_end': SelectDateWidget(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReportForm, self).__init__(*args, **kwargs)
+
+        self.fields['branch'].empty_label = '- %s -' % (_('all branches'))
+        self.fields['careworker'].empty_label = '- %s -' % (_('all careworkers'))
--- helpim/stats/models.py
+++ helpim/stats/models.py
@@ -6,29 +6,45 @@ from helpim.common.models import BranchOffice
 
 
 class Report(models.Model):
+    VARIABLE_CHOICES = (
+        ('branch', _('Branch office')),
+        ('weekday', _('Weekday')),
+    )
+
+    OUTPUT_CHOICES = (
+        ('hits', _('Hits')),
+        ('unique', _('Unique IPs'))
+    )
+
     title = models.CharField(max_length=255)
 
     period_start = models.DateField()
     period_end = models.DateField()
 
     # staff user in chat
-    branch = models.ForeignKey(BranchOffice,
-        verbose_name=_('Branch office')
+    branch = models.ForeignKey(BranchOffice, null=True, related_name='+',
+        verbose_name=_('Branch office'),
     )
-    careworker = models.ForeignKey(User,
+    careworker = models.ForeignKey(User, null=True, related_name='+',
         verbose_name=_('Careworker'),
         limit_choices_to={ 'groups__name': 'careworkers' },
     )
 
     # filter by properties of chat
-    filter_none = models.BooleanField()
-    filter_business_hours = models.BooleanField()
-    filter_blocked = models.BooleanField()
-    filter_queued = models.BooleanField()
-    filter_assigned = models.BooleanField()
-    filter_interactive = models.BooleanField()
+    filter_none = models.BooleanField(verbose_name=_('Show all'))
+    filter_business_hours = models.BooleanField(verbose_name=_('Hits outside of business hours'))
+    filter_blocked = models.BooleanField(verbose_name=_('Hits from blocked IPs'))
+    filter_queued = models.BooleanField(verbose_name=_('Hits when waiting queue was full'))
+    filter_assigned = models.BooleanField(verbose_name=_('Assigned chats'))
+    filter_interactive = models.BooleanField(verbose_name=_('Interactive chats'))
 
     # what to show in result
-    variable1 = models.CharField(max_length=255)
-    variable2 = models.CharField(max_length=255)
-    output = models.CharField(max_length=255)
+    variable1 = models.CharField(max_length=255, choices=VARIABLE_CHOICES,
+        verbose_name=_('select row variable'),
+    )
+    variable2 = models.CharField(max_length=255, choices=VARIABLE_CHOICES,
+        verbose_name=_('select column variable')
+    )
+    output = models.CharField(max_length=255, choices=OUTPUT_CHOICES, default=OUTPUT_CHOICES[0],
+        verbose_name=_('select information to show')
+    )
