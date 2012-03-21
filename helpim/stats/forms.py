632713f284cb51b8c80424bfa9b9fad4a20ae002
splatte <sp@x-berg.de>
2012-03-21 17:56:13 +0100

    sensible choices for year-selection

--- helpim/stats/forms.py
+++ helpim/stats/forms.py
@@ -1,17 +1,31 @@
+from datetime import datetime
+
 from django import forms
+from django.db.models import Max, Min
 from django.forms.extras.widgets import SelectDateWidget
 from django.utils.translation import ugettext as _
 
+from helpim.conversations.models import Chat
 from helpim.stats.models import Report
 
 
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
+
+        try:
+            # use year of first/latest Chat
+            years = Chat.objects.aggregate(Min('start_time'), Max('start_time'))
+            min_year = years['start_time__min'].year
+            max_year = years['start_time__max'].year
+        except:
+            # default to +-5 years from now if no Chats
+            min_year = datetime.now().year - 5
+            max_year = datetime.now().year + 5
+
         widgets = {
-            # TODO: use `years`:List available years in selection
-            'period_start': SelectDateWidget(),
-            'period_end': SelectDateWidget(),
+            'period_start': SelectDateWidget(years=range(min_year, max_year + 1)),
+            'period_end': SelectDateWidget(years=range(min_year, max_year + 1)),
         }
 
     def __init__(self, *args, **kwargs):
