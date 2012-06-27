007fbf8e144a22455546dfc74fc94a1dc3c3d805
Winfried Tilanus <winfried@tilanus.com>
2012-06-27 19:55:42 +0200

    New setup_data with defaults, though in Dutch it is now a complete kickstart. Removing dump_setup_data.sh, obsoleted by management command. Updating README accordingly. closes #129

--- bin/dump_setup_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Command to use when dumping the the setup for use on an other installation
-# Usage:
-#
-# ./bin/dump_setup_data.sh > helpim/fixtures/setup_data.json
-
-`dirname $0`/manage.py dumpdata flatpages common.branchoffice auth.group --indent 4 --natural
